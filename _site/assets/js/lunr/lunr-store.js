38e4bfabf73dc320eb2a9df289fb27747b964cc5
lsirikh <lsirikh@naver.com>
2019-12-09 02:50:27 +0900

    .gitignore modification

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,56 @@
+var store = [{
+        "title": "01.ROS installation based from Ubuntu 16.04 Kinetic Kame",
+        "excerpt":"우분투 16.04 LTS버전에서 활용되는 ROS는 Kinectic Kame는 사실 우분투 16.04이기 때문에 설치되는 환경 종속적인 ROS 버전이다. 물론 Ros.org에서 말하는 위키 내용은 다소 다른 감이 있지만, 통상적으로 이렇다고 본다. ROS Kinetic Kame은 주로 Ubuntu 16.04 (Xenial) 릴리스를 대상으로하지만 Mac OS X, Android 및 Windows뿐만 아니라 다른 Linux 시스템도 다양한 정도로 지원됩니다....","categories": ["ROS"],
+        "tags": ["ROS","installation"],
+        "url": "http://localhost:4000/categories/ROS/%7Btitle%7D",
+        "teaser":null},{
+        "title": "02.ROS environmental settings for beginners ROS 환경설정 우분투 16.04 Kinetic Kame",
+        "excerpt":"1. ROS 환경설정 ROS 설치 과정에서 사용된 다음 명령어는 터미널 창을 실행할 때마다 계속 입력해야한다. $ source /opt/ros/kinetic/setup.bash $ source ~/catkin_ws/devel/setup.bash 이러한 번거로운 작업을 없애기 위해서 새로운 터미널 창을 열 때 마다 정해진 환경설정 파일을 읽어오도록 할 수 있다. 이외 ROS 네트워를 설정하고 자주 사용하는 명령어를 단축 명령어로 만들어 보자....","categories": ["ROS"],
+        "tags": ["ROS","installation"],
+        "url": "http://localhost:4000/categories/ROS/%7Btitle%7D",
+        "teaser":null},{
+        "title": "01.GIT이란 무엇인가?",
+        "excerpt":"Git 이란 무엇인가??? GitHub를 이해하려면 먼저 Git을 이해해야합니다. Git은 리눅스를 만든 사람인 Linus Torvalds에 의해 시작된 오픈 소스 버전 관리 시스템입니다. Git은 Subversion , CVS 및 Mercurial 과 같은 다른 버전 제어 시스템과 유사합니다. Git은 버전 관리 시스템이지만 무엇일까? 예를 들어, 개발자가 무언가 앱을 만들면 코드가 지속적으로 변경되어 첫 번째...","categories": ["GIT"],
+        "tags": ["GIT","introduction"],
+        "url": "http://localhost:4000/categories/GIT/%7Btitle%7D",
+        "teaser":null},{
+        "title": "03.ROS를 테스트 터틀봇 움직이기(우분투 16.04 Kinetic Kame)",
+        "excerpt":"1. ROS 동작 테스트 해보기 일단 ROS를 설치 했기 때문에 뭐라도 구동하여 ROS가 작동하는 원리를 보고 싶은 마음이 들 것이다. 나도 처음에 설치 했을 때, 로봇에 한 발자국 더 다가간 것 같았다. 일단 거북이라도 돌아다니는 걸 보고 싶다면 함께 해보는 것도 좋을 것 같다. 2. roscore 실행하기 $ roscore ROS는...","categories": ["ROS"],
+        "tags": ["ROS","turtlesim","test"],
+        "url": "http://localhost:4000/categories/ROS/%7Btitle%7D",
+        "teaser":null},{
+        "title": "04.ROS를 GUI 기반 IDE 툴 qt creator 설치(우분투 16.04 Kinetic Kame)",
+        "excerpt":"1. Qt Creator란? Qt Creator는 Qt GUI 애플리케이션 개발 프레임 워크 용 SDK의 일부인 크로스 플랫폼 C ++, JavaScript 및 QML 통합 개발 환경입니다. 시각적 디버거와 통합 GUI 레이아웃 및 양식 디자이너가 포함되어 있습니다. Qt는 컴퓨터 프로그래밍에서 GUI 프로그램 개발에 널리 쓰이는 크로스 플랫폼 프레임워크이다. 서버용 콘솔과 명령 줄 도구와...","categories": ["ROS"],
+        "tags": ["ROS","qt","creator"],
+        "url": "http://localhost:4000/categories/ROS/%7Btitle%7D",
+        "teaser":null},{
+        "title": "02.GIT 초기 설정하기",
+        "excerpt":"Git 시작하기 위한 설정하기 Git 최초 설정 Git을 설치하고 나면 Git의 사용 환경을 적절하게 설정해 주어야 한다. 환경 설정은 한 컴퓨터에서 한 번만 하면 된다. 설정한 내용은 Git을 업그레이드해도 유지된다. 언제든지 다시 바꿀 수 있는 명령어도 있다. ‘git config’라는 도구로 설정 내용을 확인하고 변경할 수 있다. Git은 이 설정에 따라...","categories": ["GIT"],
+        "tags": ["GIT","initial","commit","초기","커밋"],
+        "url": "http://localhost:4000/categories/GIT/%7Btitle%7D",
+        "teaser":null},{
+        "title": "03.GIT 시작하기에 앞서 알면 좋은 것(도움말 보기)",
+        "excerpt":"Git을 사용하기 앞서 알면 좋은 내용 Git 도움말 보기 명령어에 대한 도움말이 필요할 때 도움말을 보는 방법은 두 가지로 동일한 결과를 볼 수 있다. $ git help &lt;verb&gt; $ man git-&lt;verb&gt; 예를 들어 아래와 같이 실행하면 git config 명령에 대한 도움말을 볼 수 있다. $ git help config 도움말은 언제...","categories": ["GIT"],
+        "tags": ["GIT","시작하기","도움말"],
+        "url": "http://localhost:4000/categories/GIT/%7Btitle%7D",
+        "teaser":null},{
+        "title": "04.GIT 저장소 만들기",
+        "excerpt":"Git의 기초 - Git 저장소 만들기 Git에서 자주 사용하는 명령어는 모두 이후에 올리게 될 것이다. 추후 업데이트 되는 내용을 다 읽으면 저장소를 만들고 설정하는 방법, 파일을 추적하거나(Track) 추적을 그만두는 방법, 변경 내용을 Stage 하고 커밋하는 방법을 알게 된다. 파일이나 파일 패턴을 무시하도록 Git을 설정하는 방법, 실수를 쉽고 빠르게 만회하는 방법,...","categories": ["GIT"],
+        "tags": ["GIT","기초","저장소"],
+        "url": "http://localhost:4000/categories/GIT/%7Btitle%7D",
+        "teaser":null},{
+        "title": "05.GIT 저장소 수정하기와 저장하기",
+        "excerpt":"Git의 기초 - 수정하고 저장소에 저장하기 실제 존재하는 Git 저장소를 하나 만들었고 워킹 디렉토리에 Checkout도 했다. 이제는 파일을 수정하고 파일의 스냅샷을 커밋해 보자. 파일을 수정하다가 저장하고 싶으면 스냅샷을 커밋한다. Checkout은 처음 Git을 접하면 생소할 수 있다. Git은 스냅샷을 기반으로 데이터의 내용을 저장하고 추적한다. Checkout이 되어있는 디렉토리란(워킹디렉토리)는 현재 .git 폴더에서 관리하는...","categories": ["GIT"],
+        "tags": ["GIT","기초","수정하기","저장하기"],
+        "url": "http://localhost:4000/categories/GIT/%7Btitle%7D",
+        "teaser":null},{
+        "title": "01.CPP 학습에 앞서 CPP란 어떤 프로그램 언어일까?",
+        "excerpt":"CPP란 어떤 프로그램 언어일까? 1. 개요 C언어를 부분집합으로 갖고[1] 객체 지향 및 일반화 프로그래밍과 같은 멀티 패러다임을 지원하는 프로그래밍 언어이다. 1979년에 C언어에서 직접적으로 파생된 C with Classes라는 이름의 언어로 시작되었다가, 1983년에 지금의 이름을 갖게 되었다. 2. 특징 비아르네 스트로우스트루프(Bjarne Stroustrup)[2]가 C언어를 바탕으로 만들었다. 참고로 C 계열 언어에서 “++”라는 것은 1을...","categories": ["C++"],
+        "tags": ["C++","introduction"],
+        "url": "http://localhost:4000/categories/C++/%7Btitle%7D",
+        "teaser":null},{
+        "title": "02.C++ IDE Eclipse 설치하기",
+        "excerpt":"Eclipse IDE for C/C++ Developers 1. 자바 JDK 설치하기 사용 가능한 표준 설치 유형은 JDK와 JRE입니다. JDK (Java Development Kit)는 Java 컴파일러를 포함하여 새로운 Java 응용 프로그램을 개발할 수있는 기능을 제공합니다. JRE (Java Runtime Environment)는 애플릿이있는 Java 응용 프로그램에 대한 런타임 환경을 제공합니다. Java 개발자는 새로운 Java 응용 프로그램을 만들려면...","categories": ["C++"],
+        "tags": ["C++","introduction","이클립스","Eclipse"],
+        "url": "http://localhost:4000/categories/C++/%7Btitle%7D",
+        "teaser":null}]
